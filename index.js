fdc036749c7b3dcfedf4c2bea8c9d264894e53ec
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-27 17:41:13 +0330

    start project

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './App.css';
+// import '@material/banner/dist/mdc.banner.min.css'
+// import '@material/button/dist/mdc.button.min.css'
+// import { ThemeProvider } from '@mui/material/styles'
+// import theme from './Blog-Page/Theme/Theme'
+
+
+
+
+
+ReactDOM.render(
+  
+         <App/>
+
+  ,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(); 
+
+
+//**************************************** */
+
+// har directory ye file e pish farz mitune dashte bashe ba onvane index.js
+// ke niaz nist ma un file e dakhele directory ro seda bezanim
+// ba seda kardane esme un pooshe,file e index.js dakhele un pooshe 
+// be soorate pish farz import mishe
+//exp:    ./reducers/index =====> ./reducers
+
+//************************************** */
